cf636fe1f307297fc29e12b7696c3ac965557945
Leonardo Ribeiro <48728011+LeonardoRibeiro161@users.noreply.github.com>
2020-02-04 07:40:34 -0300

    Crawling na classe class="textDiv"
    Feito uma analise no codigo fonte, é possivel obter noticias na classe class="textDiv"

--- main.py
+++ main.py
@@ -3,23 +3,17 @@ import urllib3
 import re
 from bs4 import BeautifulSoup
 
-#url = "https://br.investing.com/equities/petrobras-pn-news"
-url = "https://br.investing.com//news/stock-market-news/"
+url = "https://br.investing.com/equities/petrobras-pn-news"
+#url = "https://br.investing.com/news/stock-market-news/"
 cabecalho = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0'}
-
-
-# Invocando o metodo
 
 resposta = requests.get(url, headers=cabecalho)
 
 html = resposta.text
-#print(html)
 
 soup = BeautifulSoup(html, 'html.parser')
 
-crawling = soup.find_all('a')
-
+crawling = soup.find_all(class_="textDiv")
 
 for link in crawling:
-    #print (link.get('href'))
     print(link.get_text())
